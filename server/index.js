6362613ad3eb9e943c20410bac687e34a0de2f2e
yarinmzrc <yarinmzrc@gmail.com>
2022-04-27 11:29:45 +0300

    create db connection to mysql

--- server/index.js
+++ server/index.js
@@ -1,8 +1,33 @@
 const express = require("express");
+const mysql = require("mysql");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password'
+})
+
+db.connect(err => {
+  if(err) {
+    throw err;
+  }
+
+  console.log('MySql connected')
+})
+
+app.get('/createdb', (req, res) => {
+  let sql = 'CREATE DATABASE communication_ltd';
+  db.query(sql, err => {
+    if(err) {
+      throw err;
+    }
+    res.send('Database Created')
+  })
+})
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
