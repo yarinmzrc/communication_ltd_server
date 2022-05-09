9838c88c32f03cd473eab9301a5c312af840f7ef
yarinmzrc <yarinmzrc@gmail.com>
2022-05-09 14:05:35 +0300

    push code

--- server/index.js
+++ server/index.js
@@ -2,12 +2,33 @@ const express = require("express");
 const cors = require('cors')
 const mysql = require("mysql");
 const bcrypt = require("bcryptjs/dist/bcrypt");
+const CryptoJS = require("crypto-js");
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+let CheckPassword;
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
 app.use(cors())
+
+const genRand = (len) => {
+  return Math.random().toString(36).substring(2,len+2);
+}
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'yarinmzrc@gmail.com',
+    pass: process.env.PASSWORD_MAIL
+  }
+});
+
+
+
+
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -39,7 +60,7 @@ app.post('/create-user', async(req, res) => {
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(password, salt);
 
-      db.query(`INSERT INTO users VALUES (?,?)`, [email, hashedPassword], (err, result)=>{
+      db.query(`INSERT INTO users VALUES (?,?,?)`, [email, hashedPassword,'[]'], (err, result)=>{
         if(err) throw err;
         db.query(`SELECT * FROM users WHERE email=?`, email, (err, result) => {
           if(err) throw err;
@@ -62,10 +83,66 @@ app.post('/login-user', async(req, res) => {
       if( await bcrypt.compare(password, userFound.password)) {
         res.send(userFound);
       } else {
-        res.status(400).send("Not Authenticated");
+        res.send("Not Authenticated");
       }
     }
   })
+})
+
+app.get('/forgot-password', async(req,res) => {
+  const string = genRand(12);
+  const hash = CryptoJS.SHA1(string);
+  const result = CryptoJS.enc.Hex.stringify(hash);
+
+  var mailOptions = {
+    from: 'yarinmzrc@gmail.com',
+    to: 'yarinmzrc@gmail.com',
+    subject: 'Your New Password Is Here!',
+    text: result
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+      CheckPassword = result;
+      res.send(result)
+    }
+  });
+
+})
+
+app.post('/change-password', async (req,res) => {
+  const {email, password} = req.body;
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  let sql = `UPDATE users SET password=? WHERE email=?`;
+  db.query(sql, [hashedPassword,email], async(err, result) => {
+    if(err) throw err;
+    if(result.affectedRows === 1) {
+      db.query(`SELECT * FROM users WHERE email=?`, email, (err, result) => {
+        if(err) throw err;
+        if(result){
+          res.send(result[0])
+        }
+      })
+    }
+  })
+
+})
+
+app.post('/add-customer', async(req,res) => {
+  const {customerData, email} = req.body;
+  const customers = JSON.stringify(customerData);
+  let sql = `UPDATE users SET customers=? WHERE email=?`;
+  db.query(sql, [customers,email], async(err,result) =>{
+    if(err) throw err;
+    if(result) {
+      res.send(result);
+    }
+  })
+
 })
 
 app.listen(PORT, () => {
@@ -73,7 +150,7 @@ app.listen(PORT, () => {
 });
 
 const createTables = () => {
-  let sql = 'CREATE TABLE IF NOT EXISTS users (email VARCHAR(50), password TEXT)';
+  let sql = 'CREATE TABLE IF NOT EXISTS users (email VARCHAR(50), password TEXT, customers JSON)';
   db.query(sql, (err, result) => {
     if(err){
       throw err;
